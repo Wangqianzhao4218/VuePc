1ce7486a77fc6f6431459a7e53b0efe7a4410c49
Wangqianzhao4218 <764488971@qq.com>
2019-10-25 21:22:01 +0800

    登录界面提交

---
+++ src/views/login/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <!-- 登录卡片 -->
+    <el-card>
+      <!-- logo -->
+      <img src="../../assets/logo_index.png" width="200px" alt />
+      <el-form ref="form" :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.name" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            style="width:307px;margin-right:10px"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">立即登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: { name: '', code: '' }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.el-card {
+  width: 470px;
+  height: 370px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  /* text-align: center; */
+}
+ img {
+   display: block;
+   margin: 0 auto;
+  }
+.el-form {
+  margin-top: 30px;
+}
+</style>
